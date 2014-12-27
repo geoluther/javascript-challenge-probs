d3181d0e55a2b4fa434ecdd6792334a880efe28e
George <my latest commit>
2014-12-26 20:43:36 -0700

    refactor getRelationship

--- relationships.js
+++ relationships.js
@@ -1,42 +1,39 @@
 function getRelationship(x, y) {
-// Your code goes here!
-// passed!
-var xtype = typeof x;
-var ytype = typeof y;
+	// Your code goes here!
+	// passed!
+	var xtype = getType(x);
+	var ytype = getType(y);
 
+	// console.log(xtype);
+	// console.log(ytype);
 
-if ( isNaN(x) ) {
-	xtype = "NaN";
-}
-
-if ( isNaN(y) ) {
-	ytype = "NaN";
-}
-
-// console.log(xtype);
-// console.log(ytype);
-
-
-if ( (xtype === "number") && (ytype === "number") ) {
-
-	if (x > y) {
-		return ">";
-	} else if (x < y) {
-		return "<";
-	} else {
-		return "=";
+	if ( (xtype === "number") && (ytype === "number") ) {
+		if (x > y) {
+			return ">";
+		} else if (x < y) {
+			return "<";
+		} else {
+			return "=";
+		}
 	}
 
-}
-
-	if ( (xtype !== "number") && (ytype !== "number") ){
+	if ( (xtype !== "number") && (ytype !== "number") ) {
 		return "Can't compare relationships because " + x  + " and " + y + " are not numbers";
 	} else if  ( (xtype === "number") && (ytype !== "number") ) {
 		return "Can't compare relationships because " + y + " is not a number";
 	} else {
 		return "Can't compare relationships because " + x + " is not a number";
 	}
+
 };
+
+function getType(x) {
+	if ( isNaN(x) ) {
+		return "NaN";
+	} else {
+		return typeof x;
+	}
+}
 
 
 var rel = getRelationship(2, 3);
